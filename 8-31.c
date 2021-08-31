0fbe7ec3b5ea866e130cdd561af617e4e17bfc7f
liutianqiJoey <88476491+liutianqiJoey@users.noreply.github.com>
2021-08-31 18:17:00 +0800

    Add files via upload

---
+++ 8-31.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+//void increase(int* a)
+//{
+//	*a = (*a)++;
+//}
+//int main()
+//{
+//	int k = 0;
+//	increase(&k);
+//	increase(&k);
+//		printf("%d\n",k);
+//	return 0;
+//}
+//#include<string.h>
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "one bit";
+//	strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	printf("%s\n", strcpy(arr1, arr2));
+//	/*printf("%d\n", strlen("gigantic"));*/
+//	return 0;
+//}
+int main()
+{
+	int c = Add(2, 6);
+		printf("%d\n", c);
+	return 0;
+}
