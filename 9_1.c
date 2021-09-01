8ecddecb521cc64b03d2203632f69dda6339104d
liutianqiJoey <88476491+liutianqiJoey@users.noreply.github.com>
2021-09-01 20:32:35 +0800

    Add files via upload

---
+++ 9_1.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+//void print(unsigned int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n%10);
+//
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%u", &num);//%u is unsigned int
+//	print(num);
+//	return 0;
+//}
+//int main()
+//{
+//	/*char arr[] = { 'a','b','c','\0'};
+//	printf("%d ", strlen(arr));*/
+//	/*int l = 0;
+//	l = strlen("abcd");
+//	printf("%u ", l);*/
+//	return 0;
+//}
+//int my_strlen(char arr[])
+//{
+//	if (arr[0] != 0)
+//		return 1 + my_strlen(arr+1);
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "bit hello";
+//	printf("%d\n", my_strlen(arr1));
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int i = 0;
+//	int j = 1;
+//	scanf("%d", &a);
+//	for (i = 1; i <= a; i++)
+//	{
+//		j = j * i;
+//	}
+//	printf("%d\n", j);
+//	return 0;
+//}
+//int recursion(int a)
+//{
+//	if(a>1)
+//	return a * recursion(a - 1);
+//	else
+//	return a;
+//}
+//int main()
+//{
+//	int num = 3;
+//	scanf("%d", &num);
+//	printf("%d\n", recursion(num));
+//	return 0;
+//}
+#include <stdlib.h>
+int main()
+{
+	system("shutdown /s /t 120");
+	/*char arr[] = "abcdefg";
+	memset(arr, 'm', 3);
+	printf("%s\n", arr);*/
+	return 0;
+}
